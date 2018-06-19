ec674466cdff50de0e6b71f0184b098935c68ae7
Pranav <prnvnachnekar@gmail.com>
2018-06-19 18:21:25 +0530

    -Added Quaternion class, it has the following functions 1.Add 2. Subtract 3. Multiply by scalar 4. Multiply by Quaternion 5. get Norm 6. get Unit Norm 7. get Conjugate 8. get Inverse -Added rotatevectorbyangleaboutaxis function to the vector class  which takes a angle(float) and axis(vector) as params -Fixed dot multiplication in vector class so it returns a float value -Added comments for Quaternion class

--- src/Quaternion.java
+++ src/Quaternion.java
@@ -11,10 +11,12 @@ public class Quaternion {
         s=scalar;
         v=new Vector3(vector);
     }
+    //Copy constructor
     public Quaternion(Quaternion q){
         s=q.s;
         v=new Vector3(q.v);
     }
+    //Basic Arithmetic functions
     public Quaternion add(Quaternion q){
         float resultScalar = s+q.s;
         Vector3 resultVector = v.add(q.v);
@@ -25,6 +27,8 @@ public class Quaternion {
         Vector3 resultVector = v.subtract(q.v);
         return  new Quaternion(resultScalar,resultVector);
     }
+    //Multiply function
+    //You can either multiply by a scalar or another Quaternion
     public  Quaternion multiplyBy(Quaternion q){
         float resultScalar=(s*q.s)-v.dot(q.v);
         Vector3 resultVector = ((q.v.multiplyBy(s)).add(v.multiplyBy(q.s))).add(v.cross(q.v));
@@ -35,15 +39,22 @@ public class Quaternion {
         Vector3 resultVector = v.multiplyBy(scalar);
         return new Quaternion(resultScalar,resultVector);
     }
+    //A function to get the norm of the Quaternion
+    //Norm is similar to magnitude in Vectors
     public float getNorm(){
         return (float) Math.sqrt(s*s+(v.getMagnitude()*v.getMagnitude()));
     }
+    //A function to get the unit Norm of the Quaternion
+    // Unit norm is similar to unit vector in Vector
     public Quaternion getUnitNorm(){
         return this.multiplyBy(1/this.getNorm());
     }
+    //A function to get the conjugate of the Quaternion
+    // Conjugate of a quaternion is just opposite in direction
     public Quaternion getConjugate(){
         return  new Quaternion(this.s,this.v.multiplyBy(-1));
     }
+    //A function that returns the Inverse of the Quaternion
     public Quaternion getInverse(){
         Quaternion c=getConjugate();
         float multiplicant=1/(getNorm()*getNorm());
